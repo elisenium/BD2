9d56cb9d769c05e4ac49afcc75d02b168808cfb1
elisenium <elise.grelaud@student.vinci.be>
2024-10-25 00:04:48 +0200

    edit: 2.e.iv.sql & 2.g.ii.sql

--- 2.e.iv.sql
+++ 2.e.iv.sql
@@ -1,6 +1,6 @@
 --2.e.iv
 --1 Quel est le livre le plus cher publié par l'éditeur "Algodata Infosystems" ?
-SELECT t.title_id, t.title
+SELECT t.title_id, t.title, t.price
 FROM titles t, publishers p
 WHERE t.pub_id = p.pub_id
   AND p.pub_name = 'Algodata Infosystems'
@@ -8,13 +8,21 @@ WHERE t.pub_id = p.pub_id
                  FROM titles ti
                  WHERE ti.pub_id = p.pub_id);
 
+SELECT t.title_id, t.title, t.price
+FROM titles t, publishers p
+WHERE t.pub_id = p.pub_id
+  AND p.pub_name = 'Algodata Infosystems'
+  AND t.price = (SELECT MAX(ti.price)
+                 FROM titles ti
+                 WHERE ti.pub_id = p.pub_id);
+
 --2 Quels sont les livres qui ont été vendus dans plusieurs magasins ?
 SELECT t.title_id, t.title
 FROM titles t
-WHERE t.title_id IN (SELECT sd.title_id
-                     FROM salesdetail sd, salesdetail sd2
-                     WHERE sd.stor_id <> sd2.stor_id
-                     AND sd.title_id = sd2.title_id);
+WHERE t.title_id IN (SELECT sd1.title_id
+                     FROM salesdetail sd1, salesdetail sd2
+                     WHERE sd1.stor_id <> sd2.stor_id
+                     AND sd1.title_id = sd2.title_id);
 
 SELECT t.title_id, t.title
 FROM titles t, salesdetail sd
@@ -25,7 +33,13 @@ HAVING COUNT(DISTINCT sd.stor_id) > 1;
 --3 Quels sont les livres dont le prix est supérieur à une fois et demi le prix moyen des livres du même type ?
 SELECT t.title_id, t.title
 FROM titles t
-WHERE t.price > (SELECT 1.5*avg(t2.price)
+WHERE t.price > (SELECT 1.5 * AVG(t2.price)
+                 FROM titles t2
+                 WHERE t.type = t2.type);
+
+SELECT t.title_id, t.title
+FROM titles t
+WHERE t.price > 1.5 * (SELECT AVG(t2.price)
                  FROM titles t2
                  WHERE t.type = t2.type);
 
@@ -43,16 +57,24 @@ WHERE p.state = a.state;
 
 --5 Quels sont les éditeurs qui n'ont rien édité ?
 --solution 1
-SELECT p.pub_id, p.pub_name --..
+SELECT p.pub_id, p.pub_name
 FROM publishers p
 WHERE p.pub_id NOT IN (SELECT t.pub_id
                        FROM titles t);
+
 --solution 2
+SELECT p.pub_id, p.pub_name
+FROM publishers p
+WHERE NOT EXISTS(SELECT *
+                 FROM titles t
+                 WHERE p.pub_id = t.pub_id);
+
+--solution 3
 SELECT p.pub_id, p.pub_name
 FROM publishers p LEFT JOIN titles t
     ON p.pub_id = t.pub_id
 GROUP BY p.pub_id, p.pub_name
-HAVING COUNT(title_id) = 0;
+HAVING COUNT(t.title_id) = 0;
 
 --6 Quel est l'éditeur qui a édité le plus grand nombre de livres ?
 SELECT p.pub_id, p.pub_name
@@ -62,6 +84,15 @@ GROUP BY p.pub_id, p.pub_name
 HAVING COUNT(t.title_id) >= ALL (SELECT COUNT(ti.title_id)
                                 FROM titles ti
                                 GROUP BY ti.pub_id);
+
+SELECT p.*
+FROM publishers p
+WHERE (SELECT COUNT(*)
+       FROM titles t
+       WHERE t.pub_id = p.pub_id) >= ALL (SELECT COUNT(*)
+                                          FROM publishers p1, titles t1
+                                          WHERE p1.pub_id = t1.pub_id
+                                          GROUP BY p1.pub_id);
 
 --7 Quels sont les éditeurs dont on n'a vendu aucun livre ?
 -- OK d'utiliser l'* ici car ça renvoie un booléen
@@ -100,8 +131,8 @@ FROM titles t, salesdetail sd, sales s
 WHERE t.title_id = sd.title_id
   AND s.ord_num = sd.ord_num
   AND s.stor_id = sd.stor_id
-  AND s.date = (SELECT MAX(s.date)
-                FROM sales s);
+  AND s.date = (SELECT MAX(s2.date)
+                FROM sales s2);
 
 --10 Quels sont les magasins où l'on a vendu (au moins) tous les livres vendus par le magasin "Bookbeat" ?
 --noter la correction!!
@@ -152,15 +183,12 @@ WHERE p.city IN (SELECT a.city
 
 --13 Donnez les titres des livres dont tous les auteurs sont californiens.
 --=> DISTINCT car titleauthor !!!
-SELECT DISTINCT title_id, t.title
+SELECT DISTINCT t.title_id, t.title
 FROM titles t, titleauthor ta
 WHERE t.title_id = ta.title_id
-  AND t.title_id NOT IN (SELECT ta.title_id
-                         FROM titleauthor ta
-                         WHERE ta.au_id IN (SELECT a.au_id
-                                            FROM authors a
-                                            WHERE a.state <> 'CA'
-                                            OR a.state IS NOT NULL));
+AND 'CA' = ALL (SELECT a.state
+                FROM authors a
+                WHERE a.au_id = ta.au_id);
 
 --14 Quels sont les livres qui n'ont été écrits par aucun auteur californien ?
 SELECT t.title_id, t.title
@@ -187,10 +215,17 @@ WHERE 1 = (SELECT COUNT(ta.au_id)
 
 --16 Quels sont les livres qui n'ont qu'un auteur, et tels que cet auteur soit californien ?
 SELECT t.title_id, t.title
-FROM titles t, titleauthor ta
+FROM titles t, titleauthor ta, authors a
 WHERE t.title_id = ta.title_id
-  AND ta.au_id IN (SELECT a.au_id
-                   FROM authors a
-                   WHERE a.state = 'CA'
-                     AND ta.au_id = a.au_id)
+AND ta.au_id = a.au_id
+AND a.state = 'CA'
 GROUP BY t.title_id, t.title
+HAVING COUNT(ta.au_id) = 1;
+
+SELECT DISTINCT t.title_id, t.title
+FROM titles t
+WHERE 1 = (SELECT COUNT(ta.au_id)
+                FROM titleauthor ta, authors a
+                WHERE t.title_id = ta.title_id
+                AND a.au_id = ta.au_id
+                AND a.state = 'CA');
--- 2.g.ii.sql
+++ 2.g.ii.sql
@@ -1,10 +1,11 @@
+--2.g.ii
 --1 Donnez la liste des magasins, en ordre alphabétique, en mentionnant pour chacun le chiffre d'affaire total.
 SELECT st.stor_id, st.stor_name, SUM(t.price * sd.qty) AS "chiffre_affaire_total"
 FROM stores st, salesdetail sd, titles t
 WHERE st.stor_id = sd.stor_id
   AND sd.title_id = t.title_id
 GROUP BY st.stor_id, st.stor_name
-ORDER BY stor_name;
+ORDER BY st.stor_name;
 
 --2 Donnez la liste des magasins, en mentionnant pour chacun le chiffre d'affaire total. Classez cette liste par
 -- ordre décroissant de chiffre d'affaire.
@@ -25,33 +26,27 @@ GROUP BY t.type, t.title, p.pub_name, t.price;
 
 --4 Donnez la liste des livres de plus de $20, classés par type, en donnant pour chacun son type, son titre, les noms
 -- de ses auteurs et son prix.
-SELECT t.type, t.title, p.pub_name, a.au_fname, a.au_lname, t.price
-FROM titles t, publishers p, titleauthor ta, authors a
-WHERE t.pub_id = p.pub_id
-  AND ta.title_id = t.title_id
-  AND a.au_id = ta.au_id
-GROUP BY t.type, t.title, p.pub_name, a.au_fname, a.au_lname, t.price;
+SELECT t.title_id, title, type,price, a.au_id,a.au_lname, a.au_fname
+FROM titles t LEFT OUTER JOIN titleauthor ta
+ON t.title_id = ta.title_id LEFT OUTER JOIN  authors a
+ON ta.au_id = a.au_id
+AND t.price > 20
+ORDER BY t.type;
 
 --5 Quelles sont les villes de Californie où l'on peut trouver un auteur et/ou un éditeur, mais aucun magasin ?
 SELECT p.city
 FROM publishers p
 WHERE p.state = 'CA'
 UNION (SELECT a.city FROM authors a WHERE a.state = 'CA')
-EXCEPT (SELECT st.city FROM stores st WHERE st.state = 'CA')
+EXCEPT (SELECT st.city FROM stores st WHERE st.state = 'CA');
 
 --6 Donnez la liste des auteurs en indiquant pour chacun, outre son nom et son prénom, le nombre de livres de plus de
 -- 20 $ qu'il a écrits. Classez cela par ordre décroissant de nombre de livres, et, en cas d'ex aequo, par ordre
 -- alphabétique. N'oubliez pas les auteurs qui n'ont écrit aucun livre de plus de 20 $ !
-(SELECT a.au_fname, a.au_lname, count(t.title_id)
-FROM authors a, titles t, titleauthor ta
-WHERE a.au_di = ta.au_id AND ta.title_id = t.title_id
+SELECT a.au_lname, a.au_fname, COUNT(t.title_id) AS nbrLivres
+FROM authors a LEFT OUTER JOIN  titleauthor ta
+ON a.au_id = ta.au_id LEFT OUTER JOIN titles t
+ON ta.title_id = t.title_id
 AND t.price > 20
-GROUP BY a.au_fname, a.au_lname)
-UNION ALL
-(SELECT a.au_fname, a.au_lname
-FROM authors a WHERE a.au_id NOT IN (SELECT ta.au_id
-									FROM titleauthor ta2, titles t2
-									WHERE ta2.title_id = t2.title_id
-									AND t2.price > 20)
-)
-ORDER BY 3 DESC,1)
+GROUP BY a.au_lname, a.au_fname
+ORDER BY nbrLivres DESC , a.au_lname, a.au_fname;
